c6bbd40bd538bcfa54df6a877e73dca05f502ed5
Martin Hanzely <hanzely.martin@yahoo.com>
2021-09-29 22:47:07 +0200

    base inmported by almebic

--- alembic/env.py
+++ alembic/env.py
@@ -19,9 +19,10 @@ config.set_main_option("sqlalchemy.url", settings.POSTGRES_DSN)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from fpas.db.base import Base  # noqa: E402
+
+target_metadata = Base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
