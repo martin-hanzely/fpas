6cba86771957c953a55a23ea98fe0fceba870484
Martin Hanzely <hanzely.martin@yahoo.com>
2021-09-30 12:05:08 +0200

    added Item schema

---
+++ fpas/schemas/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+
+from fpas.schemas.item import Item, ItemCreate, ItemUpdate
---
+++ fpas/schemas/item.py
@@ -0,0 +1,30 @@
+import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel): ...
+
+
+class ItemCreate(ItemBase):
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+
+
+class ItemUpdate(ItemBase):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    is_active: Optional[bool] = None
+
+
+class Item(ItemBase):
+    id: int
+    name: str
+    description: Optional[str] = None
+    is_active: bool
+    created_timestamp: datetime.datetime
+
+    class Config:
+        orm_mode = True
