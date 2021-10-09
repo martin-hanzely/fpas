9b64fbdd5c53a1fc242911667181a8673fcedf31
Martin Hanzely <hanzely.martin@yahoo.com>
2021-10-09 20:07:54 +0200

    used test_item fixture instead of creating items

--- tests/test_crud/test_crud_item.py
+++ tests/test_crud/test_crud_item.py
@@ -3,6 +3,7 @@ from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from fpas import crud
+from fpas.models.item import Item
 from fpas.schemas.item import ItemCreate, ItemUpdate
 
 
@@ -15,18 +16,16 @@ async def test_create_item(get_db: AsyncSession) -> None:
 
 
 @pytest.mark.asyncio
-async def test_create_duplicate_item(get_db: AsyncSession) -> None:
-    await crud.item.create(get_db, obj=ItemCreate(name="Test Item"))
+async def test_create_duplicate_item(get_db: AsyncSession, test_item: Item) -> None:
     with pytest.raises(IntegrityError):
-        await crud.item.create(get_db, obj=ItemCreate(name="Test Item"))
+        await crud.item.create(get_db, obj=ItemCreate(name=test_item.name))
 
 
 @pytest.mark.asyncio
-async def test_read_one_item_by_id(get_db: AsyncSession) -> None:
-    item_created = await crud.item.create(get_db, obj=ItemCreate(name="Test Item"))
-    item_read = await crud.item.read_one(get_db, id=item_created.id)
+async def test_read_one_item_by_id(get_db: AsyncSession, test_item: Item) -> None:
+    item_read = await crud.item.read_one(get_db, id=test_item.id)
     assert item_read
-    assert item_created.name == item_read.name
+    assert test_item.name == item_read.name
 
 
 @pytest.mark.asyncio
@@ -36,11 +35,10 @@ async def test_read_nonexistent_by_id(get_db: AsyncSession) -> None:
 
 
 @pytest.mark.asyncio
-async def test_read_items(get_db: AsyncSession) -> None:
-    item_created = await crud.item.create(get_db, obj=ItemCreate(name="Test Item"))
+async def test_read_items(get_db: AsyncSession, test_item: Item) -> None:
     items = await crud.item.read(get_db)
     assert len(items) == 1
-    assert item_created.id in [i.id for i in items]
+    assert test_item.id in [i.id for i in items]
 
     item_created_2 = await crud.item.create(get_db, obj=ItemCreate(name="Test Item 2"))
     items = await crud.item.read(get_db)
@@ -49,11 +47,10 @@ async def test_read_items(get_db: AsyncSession) -> None:
 
 
 @pytest.mark.asyncio
-async def test_update_item(get_db: AsyncSession) -> None:
-    item_created = await crud.item.create(get_db, obj=ItemCreate(name="Test Item"))
+async def test_update_item(get_db: AsyncSession, test_item: Item) -> None:
     update_data = ItemUpdate(name="Test Item 2")
-    await crud.item.update(get_db, id=item_created.id, obj=update_data)
-    item_read = await crud.item.read_one(get_db, id=item_created.id)
+    await crud.item.update(get_db, id=test_item.id, obj=update_data)
+    item_read = await crud.item.read_one(get_db, id=test_item.id)
     assert item_read
     assert update_data.name == item_read.name
 
@@ -65,10 +62,9 @@ async def test_update_nonexistent(get_db: AsyncSession) -> None:
 
 
 @pytest.mark.asyncio
-async def test_delete_item(get_db: AsyncSession) -> None:
-    item_created = await crud.item.create(get_db, obj=ItemCreate(name="Test Item"))
+async def test_delete_item(get_db: AsyncSession, test_item: Item) -> None:
     items = await crud.item.read(get_db)
     assert len(items) == 1
-    await crud.item.delete(get_db, id=item_created.id)
+    await crud.item.delete(get_db, id=test_item.id)
     items = await crud.item.read(get_db)
     assert len(items) == 0
