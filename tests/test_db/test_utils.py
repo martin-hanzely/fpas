d68c3803e9c01fbfc9bc41554fe88dbc32bb4cd0
Martin Hanzely <hanzely.martin@yahoo.com>
2021-10-22 12:08:59 +0200

    cosmetics

--- tests/test_db/test_utils.py
+++ tests/test_db/test_utils.py
@@ -8,9 +8,10 @@ from fpas.db.utils import async_postgres_dsn
 @pytest.mark.parametrize(
     "input,expected_output",
     [
-        (
+        pytest.param(
             "postgresql://abc:def@localhost:5432/123",
             "postgresql+asyncpg://abc:def@localhost:5432/123",
+            id="valid dsn scheme"
         ),
         pytest.param(
             "mysql://abc:def@localhost:5432/123",
